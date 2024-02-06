b9d79df822a5af45974b2386f0d1dc1faa9e0c14
xAm64 <48089283+xAm64@users.noreply.github.com>
2024-02-06 14:02:37 +0100

    Ajout class main

---
+++ src/main/java/fr/fms/Application.java
@@ -0,0 +1,11 @@
+package fr.fms;
+
+import java.util.Scanner;
+
+import javax.sound.midi.VoiceStatus;
+
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
--- src/main/java/fr/fms/dao/testDao.java
+++ src/main/java/fr/fms/dao/testDao.java
@@ -3,8 +3,7 @@ package fr.fms.dao;
 public class testDao {
 
 	public static void main(String[] args) {
-		System.out.println("test");
-
+		
 	}
 
 }
