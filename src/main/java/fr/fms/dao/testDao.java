b673e2ac49c4fd18039756f408081bfe9af46dc4
xAm64 <48089283+xAm64@users.noreply.github.com>
2024-02-06 13:53:31 +0100

    Déplacement du test

---
+++ src/main/java/fr/fms/dao/testDao.java
@@ -0,0 +1,10 @@
+package fr.fms.dao;
+
+public class testDao {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}
--- src/main/java/tests.java
+++
@@ -1,9 +0,0 @@
-
-public class tests {
-
-	public static void main(String[] args) {
-		System.out.println("Test");
-
-	}
-
-}
