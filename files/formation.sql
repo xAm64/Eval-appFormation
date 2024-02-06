aaa5a214cadfbedc9a9993c1405c393d89c5f504
xAm64 <48089283+xAm64@users.noreply.github.com>
2024-02-06 11:15:52 +0100

    Mise a jour SQL et TEST

--- files/formation.sql
+++ files/formation.sql
@@ -5,36 +5,33 @@ USE FormationApp;
 
 -- Créer les 3 Tables
 CREATE TABLE Formation(
-   idFormation 		INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+   idFormation 		INT unsigned PRIMARY KEY AUTO_INCREMENT,
    titre 			VARCHAR(55) NOT NULL,
-   prix CURRENCY 	FLOAT(8) NOT NULL,
+   prix 			FLOAT(8) NOT NULL,
    description 		VARCHAR(250) NOT NULL,
-   lieu 			VARCHAR(50) NOT NULL
+   lieu 			VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE Utilisateur(
-   idUser 		INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+   idUser 		INT unsigned PRIMARY KEY AUTO_INCREMENT,
    nom 			VARCHAR(50) NOT NULL,
    email 		VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Commande(
-   idCommande 	INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+   idCommande 	INT unsigned PRIMARY KEY AUTO_INCREMENT,
    dateCommande DATE NOT NULL,
    prixTotal 	FLOAT(8) NOT NULL,
-   idFormation 	INT UNSIGNED NOT NULL,
-   idUser 		INT UNSIGNED NOT NULL,
-   PRIMARY KEY(idCommande),
+   idFormation 	INT unsigned NOT NULL,
+   idUser 		INT unsigned NOT NULL,
    FOREIGN KEY(idFormation) REFERENCES Formation(idFormation),
    FOREIGN KEY(idUser) REFERENCES Utilisateur(idUser)
 );
 
 -- Ajouter des formations dans la base
-INSERT INTO Formation (titre, prix, description, lieu) VALUES
-	("Designer graphique", 2783.90, "Apprenner a maitriser les outils graphiques...", "Présentiel 2jours par semaines, distanciel le reste: Adresse Champs élyséees Paris" ),
-	("Sketchup", 2775.85, "Apprennez a maitriser l'outil Google Sketchup", "Distanciel à 100%"),
-	("Manutensionnaire", 753.21, "Vous apprends a porter des lourdes charges de façon sécurisée, et a manipuler les machines de façon sécurisée", "Présentiel: FMS Saint-geours-de-Maren"),
-	("Ingénieur informatique", 32863.32, "Formation en alternance 6semaines entreprise 2 semaines en présentiel, donnant accès a un titre profesionnel", "Alternance en entreprise et présentiel: Centre de réadaptation de Mulhouse, à Mulhouse")
-;
+INSERT INTO Formation (titre, prix, description, lieu) VALUES ('Designer graphique', 2783.90, 'Apprenner a maitriser les outils graphiques...', 'Présentiel 2jours par semaines.' ),
+	('Sketchup', 2775.85, 'Apprennez a maitriser l’outil Google Sketchup', 'Distanciel à 100%'),
+	('Manutentionnaire', 753.21, 'Vous apprends a porter des lourdes charges de façon sécurisée, et a manipuler les machines de façon sécurisée', 'Présentiel'),
+	('Ingénieur informatique', 32863.32, 'Formation en alternance 6semaines entreprise 2 semaines en présentiel, donnant accès a un titre profesionnel', 'Présentiel');
 
-SELECT * FORM Formation;
+SELECT * FROM Formation;
